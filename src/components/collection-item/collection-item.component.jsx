25a44dcc8e87c5f4acb4fbff5305c940a4eb60d3
hadipouranian <hadi.pouranian@gmail.com>
2021-07-01 09:44:31 +0800

    some styles changes

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -11,9 +11,13 @@ const CollectionItem = ({ item, addItem }) => {
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="collection-footer">
         <span className="name">{name}</span>
-        <span className="name">{price}</span>
+        <span className="price">{price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButton
+        className="custom-button"
+        onClick={() => addItem(item)}
+        inverted
+      >
         ADD TO CART
       </CustomButton>
     </div>
